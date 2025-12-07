2e90f70e45cba34b6fbd108288dd41ade62ec9b4
Andrey <159928617+dissimilated2003@users.noreply.github.com>
2025-12-07 17:42:01 +0300

    возможно читать

---
+++ pascal-samples/!MainFeatures/01_First/Amp.pas
@@ -0,0 +1,12 @@
+﻿// Пример иллюстрирует использование знака "&" для снятия атрибута ключевого слова
+
+var 
+  &begin,&end: integer;  
+  i: integer;
+
+begin
+  &begin := 1;
+  &end := 2;
+  var t: System.Type := &begin.GetType; // в System.Type использовать & не надо
+  write(&begin,' ',&end,' ',t);
+end.
